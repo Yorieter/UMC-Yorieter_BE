e8c812e96f035b12a117b6aec796941ed135907e
zzimnii <102315673+zzimnii@users.noreply.github.com>
2024-07-18 00:09:45 +0900

    [chore] initial project setup

---
+++ src/main/java/umc/yorieter/YorieterApplication.java
@@ -0,0 +1,13 @@
+package umc.yorieter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YorieterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YorieterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/umc/yorieter/YorieterApplicationTests.java
@@ -0,0 +1,13 @@
+package umc.yorieter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YorieterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
