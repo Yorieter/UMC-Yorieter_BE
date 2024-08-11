184b088b55873d9faa233d05877b14425bd19e4d
zzimnii <102315673+zzimnii@users.noreply.github.com>
2024-08-11 22:19:58 +0900

    [feat] 회원가입 기본값 설정 #62

--- src/main/java/umc/yorieter/service/AuthService/AuthServiceImpl.java
+++ src/main/java/umc/yorieter/service/AuthService/AuthServiceImpl.java
@@ -2,6 +2,7 @@ package umc.yorieter.service.AuthService;
 
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.core.Authentication;
@@ -17,6 +18,7 @@ import umc.yorieter.web.dto.request.MemberSignupRequestDTO;
 import umc.yorieter.web.dto.response.MemberResponseDTO;
 import umc.yorieter.web.dto.response.TokenDTO;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class AuthServiceImpl implements AuthService {
@@ -38,8 +40,16 @@ public class AuthServiceImpl implements AuthService {
                     throw new GeneralException(ErrorStatus.USERNAME_ALREADY_EXISTS, errorMessage);
                 });
 
-        Member entity = memberRepository.save(memberSignupRequestDto.toEntity(passwordEncoder));
-        return new MemberResponseDTO(entity);
+        Member member = memberSignupRequestDto.toEntity(passwordEncoder);
+
+        // 기본 프로필 이미지 설정
+        String defaultProfileImageUrl = "https://umc-yorieter.s3.ap-northeast-2.amazonaws.com/default.png"; // 실제 경로로 대체
+        member.updateProfileUrl(defaultProfileImageUrl);
+
+        // Member와 MemberProfile 저장
+        memberRepository.save(member);
+
+        return new MemberResponseDTO(member);
     }
 
     @Transactional
