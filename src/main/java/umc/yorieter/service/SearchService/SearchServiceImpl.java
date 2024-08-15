9de4c92d6c932ee1a090d90b8d1691885b5c9d07
hyeonjichoi21 <ch5i_hj15@naver.com>
2024-08-15 18:06:23 +0900

    [fix] 검색 조회 시 기본 이미지 설정

--- src/main/java/umc/yorieter/service/SearchService/SearchServiceImpl.java
+++ src/main/java/umc/yorieter/service/SearchService/SearchServiceImpl.java
@@ -35,7 +35,6 @@ public class SearchServiceImpl implements SearchService {
         Integer minCalorie = searchRequestDTO.getMinCalorie();
         List<String> ingredientNames = searchRequestDTO.getIngredientNames();
 
-        log.info("Search Request - Max Calorie: {}, Min Calorie: {}, Ingredient Names: {}", maxCalorie, minCalorie, ingredientNames);
 
 
         // 1. 식재료 이름으로 ingredient_id 찾기
@@ -45,7 +44,7 @@ public class SearchServiceImpl implements SearchService {
                         .orElse(null))
                 .filter(Objects::nonNull)
                 .toList();
-        log.info("Ingredients: {}", ingredientIds);  // 식재료 이름 찾는 메소드
+
 
         // 2. ingredient_id로 recipe_id 찾기
         List<Long> recipeIds = ingredientIds.stream()
@@ -54,18 +53,17 @@ public class SearchServiceImpl implements SearchService {
                 .distinct()
                 .collect(Collectors.toList());
 
-        log.info("Recipe IDs: {}", recipeIds);
+
 
         // 3. recipe_id로 Recipe 엔티티 찾기
         List<Recipe> recipes = recipeRepository.findByIdIn(recipeIds);
-        log.info("Recipe IDs: {}", recipeIds);
+
 
         // 필터링: 칼로리 범위에 맞는 레시피만 선택
         List<Recipe> filteredRecipes = recipes.stream()
                 .filter(recipe ->recipe.getCalories()!= null &&  recipe.getCalories() >= minCalorie && recipe.getCalories() <= maxCalorie)
                 .toList();
 
-        log.info("Filtered Recipes Count: {}", filteredRecipes.size());
 
 
         // DTO 변환
@@ -77,13 +75,15 @@ public class SearchServiceImpl implements SearchService {
                             .map(recipeIngredient -> recipeIngredient.getIngredient().getName()) // 각 재료의 이름을 추출
                             .collect(Collectors.toList());
 
+                    String imageUrl = recipe.getRecipeImage() != null ? recipe.getRecipeImage().getUrl() : "https://umc-yorieter.s3.ap-northeast-2.amazonaws.com/default.png";
+
                     return SearchResponseDTO.DetailRecipeDTO.builder()
                             .recipeId(recipe.getId())
                             .memberId(recipe.getMember().getId())
                             .title(recipe.getTitle())
                             .description(recipe.getDescription())
                             .calories(calories)
-                            .imageUrl(recipe.getRecipeImage().getUrl())
+                            .imageUrl(imageUrl) // recipe.getRecipeImage().getUrl()
                             .ingredientNames(recipeIngredientNames) // 추출한 식재료 이름 리스트 사용
                             .createdAt(recipe.getCreatedAt())
                             .updatedAt(recipe.getUpdatedAt())
@@ -91,7 +91,6 @@ public class SearchServiceImpl implements SearchService {
                 })
                 .collect(Collectors.toList());
 
-        log.info("Detail Recipe DTOs Count: {}", detailRecipeDTOS.size());
 
 
         return SearchResponseDTO.AllRecipeListDto.builder()
@@ -110,6 +109,7 @@ public class SearchServiceImpl implements SearchService {
         List<Recipe> recipes = recipeRepository.findByTitleContaining(title);
 
 
+
         // DTO 변환
         List<SearchResponseDTO.DetailRecipeDTO> detailRecipeDTOS = recipes.stream()
                 .map(recipe -> {
@@ -117,13 +117,15 @@ public class SearchServiceImpl implements SearchService {
                             .map(recipeIngredient -> recipeIngredient.getIngredient().getName())
                             .collect(Collectors.toList());
 
+                    String imageUrl = recipe.getRecipeImage() != null ? recipe.getRecipeImage().getUrl() : "https://umc-yorieter.s3.ap-northeast-2.amazonaws.com/default.png";
+
                     return SearchResponseDTO.DetailRecipeDTO.builder()
                             .recipeId(recipe.getId())
                             .memberId(recipe.getMember().getId())
                             .title(recipe.getTitle())
                             .description(recipe.getDescription())
                             .calories(recipe.getCalories())
-                            .imageUrl(recipe.getRecipeImage().getUrl())
+                            .imageUrl(imageUrl) // recipe.getRecipeImage().getUrl()
                             .ingredientNames(recipeIngredientNames)
                             .createdAt(recipe.getCreatedAt())
                             .updatedAt(recipe.getUpdatedAt())
