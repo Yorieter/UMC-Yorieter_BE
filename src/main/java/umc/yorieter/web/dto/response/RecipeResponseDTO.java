515ec63e142a614949d7a4893b74d655055c7bca
hyeonjichoi21 <ch5i_hj15@naver.com>
2024-08-23 00:35:41 +0900

    [fix] 좋아요 유무 추가

--- src/main/java/umc/yorieter/web/dto/response/RecipeResponseDTO.java
+++ src/main/java/umc/yorieter/web/dto/response/RecipeResponseDTO.java
@@ -27,6 +27,7 @@ public class RecipeResponseDTO {
         private Integer calories;
         private String imageUrl;
         private List<String> ingredientNames; // 식재료 리스트 추가
+        private boolean isLiked; // 좋아요 여부 추가
 
         @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
         private LocalDateTime createdAt;
